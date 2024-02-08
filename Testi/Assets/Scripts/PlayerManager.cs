ef754ccefd39c9c8d2f065ceb6017b95332f478f
Mat5u <118376572+Mat5u@users.noreply.github.com>
2024-02-08 13:29:36 +0200

    Level 8 valmis.

--- Testi/Assets/Scripts/PlayerManager.cs
+++ Testi/Assets/Scripts/PlayerManager.cs
@@ -5,13 +5,22 @@ using TMPro;
 using UnityEngine.UI;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class PlayerManager : MonoBehaviour
 {
 
+    public bool normalMode = true;
     public float moveSpeed = 5f;
+    public float jumpingPower = 10f;
 
     Vector2 movement;
+
+    private float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
     public Rigidbody2D rb;
     //public Animator animator;
@@ -20,7 +29,6 @@ public class PlayerManager : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,17 +38,43 @@ public class PlayerManager : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        //animator.SetFloat("Horizontal", movement.x);
-        //animator.SetFloat("Vertical", movement.y);
-        //animator.SetFloat("Speed", movement.sqrMagnitude);
+        //Jump
+        if (isGrounded() && !normalMode)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else if (!isGrounded() && !normalMode)
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
 
+
+
+        if (Input.GetKeyDown(KeyCode.W) && coyoteTimeCounter > 0f && !normalMode)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && rb.velocity.y > 0f && !normalMode)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+
+            coyoteTimeCounter = 0f;
+        }
 
     }
 
     private void FixedUpdate()
     {
         //Movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (normalMode == true)
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+        
+        else if(normalMode == false){
+            rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
+        }
 
     }
 
@@ -59,5 +93,29 @@ public class PlayerManager : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ChangeMode"))
+        {
+            rb.gravityScale = 1;
+            normalMode = false;
+        }  
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ChangeMode"))
+        {
+            Debug.Log("Vaihda modea");
+            rb.gravityScale = 0;
+            normalMode = true;
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 }
