24ecdc8d3581663a60bf9691fe26045a4e4d5ea0
Mat5u <118376572+Mat5u@users.noreply.github.com>
2024-03-13 13:18:46 +0200

    Timer

---
+++ Testi/Assets/Timer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] public TextMeshProUGUI timerText;
+    float elapsedTime;
+    int level = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+       level = SceneManager.GetActiveScene().buildIndex - 2;
+    }
+}
